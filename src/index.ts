814ef0322c81029e4cf92156454fbb69d4c8a3e3
unknown <vanspauwensebastiaan@gmail.com>
2022-08-16 19:58:31 +0200

    Change package name, update to 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import * as fs from 'fs'
 // import * as yargs from 'yargs'
 
 function createFolderStructure (name: string): any {
+  name = name.toLowerCase()
   const folders = {
     dto: {},
     models: {},
     services: {},
     transformers: {}
   } as unknown as any
+
   folders.dto = [`create-${name}.dto.ts`, `update-${name}.dto.ts`]
   folders.models = [`${name}.model.ts`]
   folders.services = [`${name}.service.ts`]
@@ -33,6 +35,7 @@ function _throw (): never {
 async function generate (): Promise<void> {
   const currentDir = process.cwd()
   const componentName = process.argv.slice(2)[0] ?? _throw()
+  const lowercase = componentName.toLowerCase()
 
   const template = `${currentDir}/src/templates`
   const folderPath = `${currentDir}/src/components/${componentName}`
@@ -43,7 +46,7 @@ async function generate (): Promise<void> {
   for (const key in structure) {
     const folder = structure[key]
     for (const file of folder) {
-      fs.mkdirSync(`${folderPath}/${key}`, { recursive: true })
+      fs.mkdirSync(`${folderPath}/${key}`, { recursive: true });
 
 
       const filePath = `${folderPath}/${key}/${file}`
@@ -53,5 +56,11 @@ async function generate (): Promise<void> {
     }
   }
 
-  // Create single files
+  const singleFiles = ['controller', 'router', 'middleware']
+  for (const file of singleFiles) {
+    const filePath = `${folderPath}/${lowercase}.${file}.ts`
+    const text = readAndReplace(`${template}/${file}.template.hbs`, componentName)
+
+    fs.writeFileSync(filePath, text)
+  }
 }
